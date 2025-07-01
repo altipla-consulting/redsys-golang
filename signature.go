2544b098d34adba91a1b652a8d9071d4880a522c
Ernest Neller <ernest.neller@altiplaconsulting.com>
2025-07-01 12:01:15 +0200

    Add 9590 to the list of cancelled responses. (#15)

--- signature.go
+++ signature.go
@@ -336,6 +336,7 @@ func Confirm(ctx context.Context, secret string, signed Signed) (Operation, erro
 		9673, // Bizum cancellation.
 		9754, // Card has not enabled PSD2 in the bank.
 		9915, // Cancelled by the user.
+		9590, // EMV3DS authentication operation rejected, error when dismantling the CRes response.
 	}
 	switch {
 	case params.Response == 913 || params.Response == 9051:
